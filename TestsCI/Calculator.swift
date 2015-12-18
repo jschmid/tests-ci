e5d743ccbc98ee454ee960c0baaa18008a717fc0
Jonas Schmid <jonas.schmid@gmail.com>
2015-12-18 15:13:31 +0100

    Added simple Calculator

---
+++ TestsCI/Calculator.swift
@@ -0,0 +1,15 @@
+//
+//  Calculator.swift
+//  TestsCI
+//
+//  Created by Jonas Schmid on 18/12/15.
+//  Copyright © 2015 schmid. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    func add(left: Int, _ right: Int) -> Int {
+        return left + right
+    }
+}
--- TestsCITests/TestsCITests.swift
+++ TestsCITests/TestsCITests.swift
@@ -21,16 +21,10 @@ class TestsCITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testAdd() {
+        let calculator = Calculator()
+        let result = calculator.add(1, 2)
+        XCTAssertEqual(result, 3)
     }
     
 }
