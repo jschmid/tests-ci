f4d4187c2109365570a0cf74358c47d676be396e
Jonas Schmid <jonas.schmid@gmail.com>
2016-05-19 11:45:58 +0200

    Improve coverage 🐵

--- TestsCI/ViewController.swift
+++ TestsCI/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage.Asset.Monkey.image
+        let imageView = UIImageView(image: image)
+        
+        self.view.addSubview(imageView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
